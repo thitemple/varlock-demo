edeebae4c24f7ec7eebd8986dea87a4233b6cf6f
Thiago Temple <thitemple@icloud.com>
2026-04-12 19:25:50 -0400

    Demo ready

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,13 @@
 import { env } from '$env/dynamic/private';
+import { ENV } from 'varlock/env';
 
 export async function load() {
-	const { DATABASE_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = env;
+	const { DATABASE_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, IS_TEST } = env;
 
 	return {
-		DATABASE_URL,
-		GOOGLE_CLIENT_ID,
-		GOOGLE_CLIENT_SECRET
+		DATABASE_URL: ENV.DATABASE_URL,
+		GOOGLE_CLIENT_ID: ENV.GOOGLE_CLIENT_ID,
+		GOOGLE_CLIENT_SECRET: ENV.GOOGLE_CLIENT_SECRET,
+		isTest: ENV.IS_TEST
 	};
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { varlockVitePlugin } from '@varlock/vite-integration';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [varlockVitePlugin(), sveltekit()]
 });
